2b1f68888364ea5a46bc15558b82ea312e2ee683
ryan rivera <rrmatthew03@yahoo.com>
2022-03-25 17:38:43 -0700

    commit tree 2f93d2ec2db7f315775f90bddb48dcffff287395

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Code your solution in this file!
+
+// => pickup on 43 street taken to 42 street 
+function distanceFromHqInBlocks(someValue) {
+    return '1';
+}
+distanceFromHqInBlocks('1')
+
+// => pickup from 50 street to 42 street
+function distanceFromHqInBlocks(number){
+    return 8;
+}
+distanceFromHqInBlocks('8')
+
+// => calculates distances below 42nd street to 34 street
+function subtractDistanceFromHqInBlocks(x, y){
+    console.log(subtractDistanceFromHqInBlocks(42, 34))
+}
+
+// => one block in downtown is 264 ft
+function distanceFromHqInFeet(someValue){
+    console.log(distanceFromHqInFeet(43))
+    return 264
+}
+distanceFromHqInFeet('264)')
+
+// => distance in feet from 50 street to 42 street
+function distanceFromHqInFeet(someValue){
+    return 2112
+}
+distanceFromHqInFeet('2112')
+
+// => calculates distances below 42nd street to 34 street
+function distanceFromHqInFeet(someValue){
+    return 2112
+}
+distanceFromHqInFeet('2112')
+
+// => distance travelled from 34 street to 48 street
+function distanceTravelledInFeet(someValue){
+    return 1320
+}
+distanceTravelledInFeet('1320')
+
+// => distance travelled from 50 street to 60 street
+function distanceTravelledInFeet(start, destination){
+    return 2640
+}
+distanceTravelledInFeet('2640')
+
+// => subtract distance from 34 street to 28 street 
+function subtractDistanceTravelledInFeet(start, destination){
+    return (subtractDistanceTravelledInFeet(34 - 28))
+}
+subtractDistanceTravelledInFeet('1584')
+
+
+function calculatesFarePrice(start, destination){
+    return (calculatesFarePrice (43, 44))
+}
+calculatesFarePrice('0')
